48a9955f7bf0b680349510277e32eaa63eb1cebe
Danny Baumann <dannybaumann@web.de>
2020-06-10 14:13:18 +0530

    Lavender: Enable call recording
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- device.mk
+++ device.mk
@@ -34,7 +34,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS := \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 

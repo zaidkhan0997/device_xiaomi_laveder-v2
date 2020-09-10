17b5d7745553af58dcc1e0dd6d02cbe4000b359b
ᴊᴏᴇ ɢᴏʟᴅʙᴇʀɢ <59208745+Hazama25@users.noreply.github.com>
2020-09-10 23:31:39 +0800

    lavender: switch to walleye oreo FP
    * this should pass cts
    
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- havoc_lavender.mk
+++ havoc_lavender.mk
@@ -32,6 +32,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=lavender \
     PRIVATE_BUILD_DESC="lavender-user 10 QKQ1.190910.002 20.3.19 release-keys"
 
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys"
+BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi

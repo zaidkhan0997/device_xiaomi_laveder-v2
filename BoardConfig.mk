9920ff56b66be5518dd8de67bd5b561cd8fb6eb9
Lucchetto <lucchetto.tie@live.com>
2020-09-18 02:16:57 +0000

    lavender: use recovery avb keys
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,8 +39,13 @@ BUILD_BROKEN_PHONY_TARGETS := true
 
 # Android Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+ifeq ($(BOARD_AVB_ENABLE), true)
+    BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+    BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
+    BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+    BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
+    BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2 --set_hashtree_disabled_flag
+endif
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 loop.max_part=16

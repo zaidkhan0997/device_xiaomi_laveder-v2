9b94df82cc529a9d8375790f68c05dc0b090d9fe
LuK1337 <priv.luk@gmail.com>
2020-09-18 10:57:02 +0200

    sdm660-common: Update qcom sepolicy include
    Change-Id: I7167575e82eb8c5de2d822643261757edda73a07
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -165,7 +165,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private

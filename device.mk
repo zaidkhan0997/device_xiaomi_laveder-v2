b7647746be68aee457125f06c507a0afc111e19e
Lucchetto <lucchetto.tie@live.com>
2020-09-18 02:18:30 +0000

    lavender: build libavservices_minijail_vendor
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- device.mk
+++ device.mk
@@ -241,6 +241,9 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_lavender
 
 # Media
+PRODUCT_PACKAGES += \
+    libavservices_minijail_vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \

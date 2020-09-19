5dca17b02f1ad671d22a1d537800d336dfd62c49
Lucchetto <lucchetto.tie@live.com>
2020-09-19 02:58:52 +0000

    lavender: fix soundtrigger hal entry
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- device.mk
+++ device.mk
@@ -31,7 +31,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.1-impl \
     audio_amplifier.sdm660 \
     audio.primary.sdm660 \
     audio.r_submix.default \

9d8450cfe818d15c7331733e3f671ead473dabff
ᴊᴏᴇ ɢᴏʟᴅʙᴇʀɢ <59208745+Hazama25@users.noreply.github.com>
2020-10-12 23:45:04 +0800

    lavender: add SystemUI to DEXPREOPT apps
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- device.mk
+++ device.mk
@@ -77,6 +77,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
+# AOT Preload
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.a2dp.default \

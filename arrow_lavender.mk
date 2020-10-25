9f084f4bac89c2b7ac9c8a8d6616ee8990086a1c
Hazama25 <hazamafawkes@gmail.com>
2020-10-25 06:52:27 +0000

    lavender: ArrowOS Bringup
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- arrow_lavender.mk
+++ arrow_lavender.mk
@@ -8,20 +8,20 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Inherit from lavender device
 $(call inherit-product, device/xiaomi/lavender/device.mk)
 
-# Inherit dalvik from device
+# Inherit dalvik from native
 $(call inherit-product, device/xiaomi/lavender/dalvikheap.mk)
 
-# Inherit some common HavocOS stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+# Inherit some common ArrowOS stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-HAVOC_BUILD_TYPE := MINKAN
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_lavender
+PRODUCT_NAME := arrow_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
@@ -30,8 +30,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=lavender \
     PRODUCT_NAME=lavender \
-    PRIVATE_BUILD_DESC="coral-user 11 RP1A.201105.002 6869500 release-keys"
+    PRIVATE_BUILD_DESC="lavender-user 10 QKQ1.190910.002 V11.0.1.0.QFGMIXM release-keys"
 
-BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi

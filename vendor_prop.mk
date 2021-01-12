b1eb0518866e192645fd32e5a874e1ab82f24f4a
CHRISL7 <70589286+ChrisW444@users.noreply.github.com>
2021-01-12 15:41:21 -0400

    sdm660-common: Add required GPU props
    These tell the ROM to use the correct Adreno drivers which fixes graphical artifacts.
    
    Credit to Sultan for the triple buffers prop.
    
    Credit to kdrag0n for noticing vulkan prop was missing.
    
    Change-Id: I3481092f3aea2be2091c8c64ca04c426a244a696

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -322,3 +322,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.zram.mark_idle_delay_mins=60 \
     ro.zram.first_wb_delay_mins=180 \
     ro.zram.periodic_wb_delay_hours=24
+
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.egl=adreno \
+    ro.hardware.vulkan=adreno

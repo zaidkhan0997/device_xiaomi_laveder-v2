93f76136eaf639a8fc3fddd26452b58142db2feb
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-01-22 05:28:19 +0000

    lavender: fix qradio and add FaceUnlock
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,5 @@
 export SKIP_ABI_CHECKS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
+git clone https://bitbucket.org/syberia-project/external_motorola_faceunlock.git -b 11.0 external/motorola/faceunlock
+git clone https://github.com/LineageOS/android_vendor_qcom_opensource_fm-commonsys -b lineage-18.1 vendor/qcom/opensource/fm-commonsys
